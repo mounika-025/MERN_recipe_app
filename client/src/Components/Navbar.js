452c2eaf69f6196ca179275900d7a9ec0a2c6600
mouni025 <im.mounika025@gmail.com>
2023-05-03 19:12:05 +0530

    added user athentication

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,14 +1,28 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
-const Navbar=()=>{
-    return(
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/create-recipe">Create Recipe</Link>
-            <Link to="/saved-recipe">Saved Recipe</Link>
-            <Link to="/auth">Login/Register</Link>
-        </div>
-    );
+const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigation = useNavigate();
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigation("/auth");
+  };
+  return (
+    <div className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/create-recipe">Create Recipe</Link>
+      <Link to="/saved-recipe">Saved Recipe</Link>
+      {!cookies.access_token ? (
+        <Link to="/auth">Login/Register</Link>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,51 +1,47 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+
+const router = express.Router();
 import { UserModel } from "../models/Users.js";
 
-const router =express.Router();
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
 
-router.post("/register",async (req, res)=>{
-    const {username,password}=req.body;
-    
-    //checking if the user is already registered
-    const user= await UserModel.findOne({username});
+  //checking if the user is already registered
+  const user = await UserModel.findOne({ username });
 
-    if (user){
-        return res.json({message:"User already registered"});
-    }
-    
-    //password hashing
-    const hashedPassword = await bcrypt.hash(password,10);
+  if (user) {
+    return res.json({ message: "User already registered" });
+  }
 
-    //adding new user to the database
-    const newUser= new UserModel({username,password:hashedPassword});
-    await newUser.save();
-    res.json({message:"User registered successfully"});
-    
-});
- 
-router.post("/login", async (req,res)=>{
-    const {username,password}=req.body;
+  //password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user= await UserModel.findOne({username});
-
-    if (!user){
-        return res.json({message:"User not found"});
-    }
-
-    //Comparing the password
-    const isPasswordValid= await bcrypt.compare(password,user.password);
-
-    if(!isPasswordValid){
-        return res.json({message:"Username or password is not valid"})
-    }
-
-    const token =jwt.sign({id: user._id},"secret");
-    res.json({token,userID:user._id});
-
-
+  //adding new user to the database
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+  res.json({ message: "User registered successfully" });
 });
 
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-export {router as userRouter};
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User not found" });
+  }
+
+  //Comparing the password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or password is not valid" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
+
+export { router as userRouter };
