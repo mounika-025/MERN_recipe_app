0ad4dc51f37f8369437b9d1a83933882f804b3d1
mouni025 <im.mounika025@gmail.com>
2023-05-03 21:40:38 +0530

    created api for new recipes

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 //generates connection towards server
 mongoose.connect(
   "mongodb+srv://mounika025:mouni2000@recipe.sasj4ba.mongodb.net/recipe?retryWrites=true&w=majority"
